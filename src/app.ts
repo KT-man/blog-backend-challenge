5b499298b47b678d799e1c65df245280455a4335
kai.ting.man <kai.ting.man@accenture.com>
2024-10-16 15:31:01 +0800

    Updating db connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
 import 'reflect-metadata';
 import Fastify from 'fastify';
+import 'dotenv/config';
+import postgres from '@fastify/postgres';
 
 const fastify = Fastify({});
+/** DB Connection */
+// Register a connection to my postgresql db with fastify
+fastify.register(postgres, {
+  connectionString: process.env.DB_URL,
+});
 
-fastify.get('/', function (request, response) {
+/** Test PG connection */
+fastify.ready().then(
+  () => {
+    console.log('Fastify is ready and connected to the database');
+  },
+  (err) => {
+    console.log('Error connecting to the database', err);
+  }
+);
+
+fastify.get('/testPing', function (request, response) {
   response.send({ hello: 'world' });
 });
 
